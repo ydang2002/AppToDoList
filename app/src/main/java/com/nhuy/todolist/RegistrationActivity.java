85425173b8a5b6942a949338965097133bf36292
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-07-07 21:11:06 +0700

    #2- Designing Login and Registration UI

--- app/src/main/java/com/nhuy/todolist/RegistrationActivity.java
+++ app/src/main/java/com/nhuy/todolist/RegistrationActivity.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
     }
 }
