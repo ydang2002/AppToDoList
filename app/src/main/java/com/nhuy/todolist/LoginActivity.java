fbab46dc4876c8025293adf238fab99c16bf1c66
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-07-09 08:11:35 +0700

    #3 - Linking Layout with Java Files

--- app/src/main/java/com/nhuy/todolist/LoginActivity.java
+++ app/src/main/java/com/nhuy/todolist/LoginActivity.java
@@ -1,16 +1,46 @@
 package com.nhuy.todolist;
 
+//import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+//import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.WindowManager;
-
 public class LoginActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private EditText loginEmail, loginPassword;
+    private Button loginBtn;
+    private TextView loginQn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        toolbar = findViewById(R.id.LoginToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Login");
+
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPassword = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginButton);
+        loginQn = findViewById(R.id.loginPageQuestion);
+
+        loginQn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
