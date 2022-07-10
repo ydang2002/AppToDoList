d76aaf66d749d5a8b011fa6321c30b1b959a7009
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2022-07-10 11:46:37 +0700

    #9-Creating a Model class

--- app/src/main/java/com/nhuy/todolist/HomeActivity.java
+++ app/src/main/java/com/nhuy/todolist/HomeActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.method.CharacterPickerDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -13,12 +15,29 @@ import android.widget.LinearLayout;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.security.ProtectionDomain;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private RecyclerView recyclerView;
     private FloatingActionButton floatingActionButton;
+
+    private DatabaseReference reference;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private String onlineUserID;
+
+    private ProgressDialog loader;
+
+    private String key = "";
+    private String task;
+    private String description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +54,12 @@ public class HomeActivity extends AppCompatActivity {
         linearLayoutManager.setStackFromEnd(true);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(linearLayoutManager);
+
+        loader = new ProgressDialog(this);
+
+        mUser = mAuth.getCurrentUser();
+        onlineUserID = mUser.getUid();
+        reference = FirebaseDatabase.getInstance().getReference().child("task").child(onlineUserID);
         
         floatingActionButton = findViewById(R.id.fab);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
